5bcfbbdabad82b6d5516a435af54bb56680ce0ff
ZanuFF <andrei.ghiura@outlook.com>
2023-12-03 16:21:20 +0200

    day3

--- 3.js
+++ 3.js
@@ -28,7 +28,27 @@ const parts = content
     ]),
         [])
 
-// console.log(parts.reduce((a,e=)))
+const gears = content
+    .split('\r\n')
+    .reduce((a, row, i) =>
+    ({
+        ...a,
+        ...(row.matchAll(new RegExp('\\*', 'gi'))
+            ? [...row.matchAll(new RegExp('\\*', 'gi'))]
+                .reduce((a, e) =>
+                (
+                    {
+                        ...a, [`${e.index}_${i}`]: []
+                    }
+                )
+                    ,
+                    {}
+                )
+            : {})
+    }), {}
+    )
+
+console.log(JSON.stringify(gears))
 
 const getNeighbours = ({ position: { x, y }, l }) => [
     { x: x - 1, y: y - 1 }, { x: x - 1, y: y }, { x: x - 1, y: y + 1 }, { x: x + l, y: y - 1 }, { x: x + l, y: y }, { x: x + l, y: y + 1 },
@@ -36,17 +56,30 @@ const getNeighbours = ({ position: { x, y }, l }) => [
     ...Array(l).fill().map((e, i) => ({ x: x + i, y: y + 1 }))
 ].filter(e => e.x > -1 & e.y > -1 & e.x < matrix[0].length & e.y < matrix.length)
 
-const checkNeighbour = ({ x, y }) => !!(matrix[y][x].replaceAll(/(\d)|\./g, '').length > 0)
+const gearNeighbour = ({ x, y }) => !!(matrix[y][x] === '*')
+
+// const checkIfValid = (part) => !!getNeighbours(part).reduce((a, e) => a | gearNeighbour(e), false)
+
+const getNeighbourGear = (part) => getNeighbours(part).reduce((a, e) => a = gearNeighbour(e) ? { ...a, [`${e.x}_${e.y}`]: [...a[`${e.x}_${e.y}`], part.number] } : a, gears)
+
+const connectedGears = parts
+    .reduce((acc, part) => Object.assign(acc, getNeighbourGear(part)), gears)
+
+const spinningGears = Object.keys(connectedGears).filter(e => connectedGears[e].length > 1)
+
+const gearRatios = spinningGears.reduce((a, e) => a + connectedGears[e].reduce((ratio, component) => ratio * parseInt(component), 1), 0)
+// PART 1
+// ------------------------------------------------------------------------------------------
+// const checkNeighbour = ({ x, y }) => !!(matrix[y][x].replaceAll(/(\d)|\./g, '').length > 0)
+// const checkIfValid = (part) => !!getNeighbours(part).reduce((a, e) => a | checkNeighbour(e), false)
+// const result = parts
+//     .reduce((a, e) => checkIfValid(e) ? [...a, parseInt(e.number)] : a, [])
+//     // .reduce((a, e) => a.indexOf(e) === -1 ? [...a, e] : a, [])
+//     .reduce((a, e) => a + e, 0)
+// ------------------------------------------------------------------------------------------
 
 
-const checkIfValid = (part) => !!getNeighbours(part).reduce((a, e) => a | checkNeighbour(e), false)
-
-parts.map(part => console.log(part.number, checkIfValid(part)))
-
-const result = parts
-    .reduce((a, e) => checkIfValid(e) ? [...a, parseInt(e.number)] : a, [])
-    // .reduce((a, e) => a.indexOf(e) === -1 ? [...a, e] : a, [])
-    .reduce((a, e) => a + e, 0)
-
-console.log(result)
+console.log(JSON.stringify(connectedGears))
+console.log(JSON.stringify(spinningGears))
+console.log(JSON.stringify(gearRatios))
 
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log([..."467..114..".matchAll(new RegExp('\\*', 'gi'))])
